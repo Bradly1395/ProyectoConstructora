23dd33a5d490f6f534885caf6f27ea3d61b6cdb8
Bradly <bradly_aguilera@yahoo.com>
2021-03-24 13:26:28 -0600

    Crear Proyecto Form

--- PlayerUI/Principal.designer.cs
+++ PlayerUI/Principal.designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace PlayerUI
 {
-    partial class Form1
+    partial class Principal
     {
         /// <summary>
         /// Required designer variable.
@@ -28,7 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Principal));
             this.panelSideMenu = new System.Windows.Forms.Panel();
             this.btnExit = new System.Windows.Forms.Button();
             this.btnHelp = new System.Windows.Forms.Button();
